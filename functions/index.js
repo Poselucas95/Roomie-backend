02cd09208defd4d9064569f7483960c07fc54576
lpose <lucas.pose@mercadolibre.com>
2020-11-01 19:02:57 -0300

    fix

--- functions/index.js
+++ functions/index.js
@@ -19,4 +19,4 @@ app.use('/api/match', matchRouter)
 app.use('/api/reject', rejectRouter)
 
 
-exports.app = functions.region("southamerica-east1").https.onRequest(app);
+exports.rumiapi = functions.region("southamerica-east1").https.onRequest(app);
