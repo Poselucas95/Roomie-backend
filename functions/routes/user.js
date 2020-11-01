262f08a7adfc7ab7f280809b76d6f600b2bc3897
lpose <lucas.pose@mercadolibre.com>
2020-11-01 19:09:01 -0300

    fix

--- functions/routes/user.js
+++ functions/routes/user.js
@@ -9,7 +9,7 @@ router.get('/:userId', async (req, res) => {
   if(req.params.userId && req.params.userId == null) res.status(400).send('Error al solicitar el user. Falta userID')
   try{
     var response = await userController.getUser(req.params.userId)
-    res.status(response.code).send("gato")
+    res.status(response.code).send(response.result)
   }catch (err){
     res.send("Error al buscar el usuario", 500)
   }
