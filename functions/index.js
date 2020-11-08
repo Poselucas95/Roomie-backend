5a5e77832e70a79ce8b49e8d398114467aa89f0e
lpose <lucas.pose@mercadolibre.com>
2020-11-08 20:19:54 -0300

    add discover

--- functions/index.js
+++ functions/index.js
@@ -7,6 +7,7 @@ var userRouter = require('./routes/user');
 var propertyRouter = require('./routes/property');
 var matchRouter = require('./routes/match');
 var rejectRouter = require('./routes/reject');
+var discoverRouter = require('./routes/discovery');
 
 app.set('etag', false)
 app.use((req, res, next) => {
@@ -17,6 +18,7 @@ app.use('/api/user', userRouter)
 app.use('/api/property', propertyRouter)
 app.use('/api/match', matchRouter)
 app.use('/api/reject', rejectRouter)
+app.use('/api/discovery', discoverRouter)
 
 
 exports.rumiapi = functions.region("southamerica-east1").https.onRequest(app);
