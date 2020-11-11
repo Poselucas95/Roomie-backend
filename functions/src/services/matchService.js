9300d02a4a90c365dcc5e78576a996d50f22c22b
lpose <lucas.pose@mercadolibre.com>
2020-11-11 19:31:51 -0300

    fix matchs

--- functions/src/services/matchService.js
+++ functions/src/services/matchService.js
@@ -21,7 +21,7 @@ async function getMatchsByUserId(userId) {
         // Parametros a insertar
         queryPrepared.input('IdFirebase', mssql.NVarChar, userId)
         // Ejecución de la query
-        await queryPrepared.query('SELECT M.*, PR.IdFirebase as idPropietario, PR.AlquilerMensual,PR.TipoHabitacion, PR.TamanoHabitacion, PR.Ciudad , PR.Barrio fp.Value as Foto, P.Nombre, P.Edad FROM Matchs M  INNER JOIN Propiedad PR ON PR.IdPropiedad = M.IdPropiedad INNER JOIN Perfil P ON P.IdFirebase = PR.IdFirebase OUTER APPLY (SELECT TOP 1 * FROM   FotoPropiedad f WHERE  M.IdPropiedad = f.IdPropiedad) fp WHERE M.IdFirebase =  @IdFirebase').then((response) => {
+        await queryPrepared.query('SELECT M.*, PR.IdFirebase as idPropietario, PR.AlquilerMensual,PR.TipoHabitacion, PR.TamanoHabitacion, PR.Ciudad , PR.Barrio, fp.Value as Foto, P.Nombre, P.Edad FROM Matchs M  INNER JOIN Propiedad PR ON PR.IdPropiedad = M.IdPropiedad INNER JOIN Perfil P ON P.IdFirebase = PR.IdFirebase OUTER APPLY (SELECT TOP 1 * FROM   FotoPropiedad f WHERE  M.IdPropiedad = f.IdPropiedad) fp WHERE M.IdFirebase =  @IdFirebase').then((response) => {
             result = response.recordset
         })
         // Cerramos la conexión
